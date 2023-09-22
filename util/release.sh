efe16499a38bb1e4d8606a9b4d4ec681b7b640ce
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-09-21 23:47:04 -0700

    add util/release.sh

---
+++ util/release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+if [ ! -z "$(git status --porcelain)" ]; then 
+    echo "You have un-committed changes. Please commit them and create a tag before releasing."
+    exit
+fi
+
+if [[ $1 = "--tag" ]]; then
+    tag=$2
+else
+    tag=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
+    if [[ $? != 0 ]]; then
+        echo "You are not currently on a tagged commit. Please create a tag before releasing."
+        exit
+    fi
+fi
+
+TLD=Interstitial_Quadrant-"$tag"
+mkdir $TLD
+cp -r ../src ../examples ../LICENSE ../README.md ../library.properties $TLD
+
+zip -r "$TLD".zip $TLD
+rm -rf $TLD
+
