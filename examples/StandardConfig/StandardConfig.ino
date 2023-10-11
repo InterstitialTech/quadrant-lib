5723cfa3530f3808242a1c3feb49086a327c2f3b
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-10-11 15:43:48 -0700

    examples/StandardConfig: print full report to USB

--- examples/StandardConfig/StandardConfig.ino
+++ examples/StandardConfig/StandardConfig.ino
@@ -3,17 +3,33 @@
   StandardConfig.ino
   Chris Chronopoulos, 2023
 
-  This sketch demonstrates the basic functionality of the board. It measures
-  all four channels and maps them to the 4 CV outputs. It also prints the
-  channel values over USB serial.
+  This sketch represents the standard, out-of-the-box configuration for
+  Quadrant. It measures all 4 lidar channels, and then (if accordingly engaged)
+  computes the following aggregate parameters:
+
+    * elevation: average height
+    * pitch: front-to-back tilt
+    * roll: side-to-side tilt
+    * arc: curvature/cupping
+
+  These parameters are then mapped to CV channels 0,1,2,3. The sensitivity of
+  the CV outputs to each parameter can be adjusted by changing the #define
+  params CV_GAIN_xx below.
+
+  The lidar values, the sample rate, and aggregate parameters are all
+  printed to USB serial in JSON format.
 
 */
+
+#define CV_GAIN_ELEVATION 1.0
+#define CV_GAIN_PITCH  3.0
+#define CV_GAIN_ROLL   4.0
+#define CV_GAIN_ARC    10.0
 
 #include "quadrant.h"
 
 Quadrant quadrant;
 
-char serial_buf[21];
 
 void setup() {
 
@@ -21,21 +37,13 @@ void setup() {
 
 }
 
+
 void loop() {
 
-  // update state machine
+  // update measurement and state variables
   quadrant.update();
 
-  // print sample rate
-  Serial.print("Sample Rate (Hz): ");
-  Serial.println(quadrant.getSampleRate());
-
-  // print lidar distances
-  sprintf(serial_buf, "%d %d %d %d", quadrant.getLidarDistance(0), quadrant.getLidarDistance(1),
-          quadrant.getLidarDistance(2), quadrant.getLidarDistance(3));
-  Serial.println(serial_buf);
-
-  // indicator leds
+  // set indicator leds
   for (int i=0; i<4; i++) {
     if (quadrant.isLidarEngaged(i)) {
       quadrant.setLed(i, HIGH);
@@ -44,39 +52,28 @@ void loop() {
     }
   }
 
-  float cv;
-
   // elevation to CV 0
   if (quadrant.isElevationEngaged()) {
-    cv = 2.5 + quadrant.getElevation();
-    quadrant.setCV(0, cv);
-    Serial.print("elevation CV: ");
-    Serial.println(cv);
+    quadrant.setCV(0, quadrant.getElevation() * 5.0 * CV_GAIN_ELEVATION);
   }
 
   // pitch to CV 1
   if (quadrant.isPitchEngaged()) {
-    cv = 2.5 + quadrant.getPitch();
-    quadrant.setCV(1, cv);
-    Serial.print("pitch CV: ");
-    Serial.println(cv);
+    quadrant.setCV(1, 2.5 + quadrant.getPitch() * 2.5 * CV_GAIN_PITCH);
   }
 
   // roll to CV 2
   if (quadrant.isRollEngaged()) {
-    cv = 2.5 + quadrant.getRoll();
-    quadrant.setCV(2, cv);
-    Serial.print("roll CV: ");
-    Serial.println(cv);
+    quadrant.setCV(2, 2.5 + quadrant.getRoll() * 2.5 * CV_GAIN_ROLL);
   }
 
   // arc to CV 3
   if (quadrant.isArcEngaged()) {
-    cv = 2.5 + quadrant.getArc();
-    quadrant.setCV(3, cv);
-    Serial.print("arc CV: ");
-    Serial.println(cv);
+    quadrant.setCV(3, 2.5 + quadrant.getArc() * 2.5 * CV_GAIN_ARC);
   }
+
+  // print to USB serial
+  Serial.println(quadrant.getReportString());
 
 }
 
