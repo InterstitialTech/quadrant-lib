ec72b610b172e4747e5002c27b7409d4b6193910
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-10-11 12:00:42 -0700

    util/install.sh: add -f option

--- util/install.sh
+++ util/install.sh
@@ -4,5 +4,12 @@
 ARDUINO_USER_DIR=$HOME/Arduino
 
 TARGET_DIR=$ARDUINO_USER_DIR/libraries/Interstitial_Quadrant
+if [[ $* == *-f* ]]; then
+    rm -r $TARGET_DIR 2> /dev/null
+fi
 mkdir $TARGET_DIR
+if [[ $? == 1 ]]; then
+    echo "To overwrite existing library, use $0 -f"
+    exit
+fi
 cp -r ../src ../examples ../LICENSE ../README.md ../library.properties $TARGET_DIR
